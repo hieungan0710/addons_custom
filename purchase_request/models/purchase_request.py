ca2431a9550fd41b606cd68b6f4b7cb54eedceeb
hieungan0710 <160987869+hieungan0710@users.noreply.github.com>
2024-03-28 17:30:47 +0700

    Add files via upload

--- purchase_request/models/purchase_request.py
+++ purchase_request/models/purchase_request.py
@@ -11,12 +11,22 @@ class PurchaseRequest(models.Model):
     date_approve = fields.Date(string='Ngày phê duyệt' )
     request_line_ids = fields.One2many('purchase.request.line', 'request_id')
     description = fields.Text(string='Mô tả')
-    state = fields.Selection([('draft', 'Draft'), ('wait', 'Wait'), ('approved', 'Approved'), ('cancel', 'Cancel')],
-                             string='Trạng thái', default='draft')
+    state = fields.Selection([('draft', 'Dự Thảo'), ('wait', 'Chờ duyệt'), ('approved', 'Đã phê duyệt'), ('cancel', 'Hoàn thành')],
+                             string='Trạng thái', default='draft',readonly=True)
     total_qty = fields.Float(string='Tổng số lượng', compute='_compute_total_qty', store=True)
     total_amount = fields.Float(string='Tổng giá trị', compute='_compute_total_amount', store=True)
     have_write_right = fields.Boolean(string='Have write right', compute="_compute_have_write_right")
     go = fields.Boolean(string="create", default=False)
+
+    def returns(self):
+        self.write({'state': "wait"})
+        return self.env.ref('purchase.report_purchase_quotation')
+    def QL(self):
+        self.write({'state': "draft"})
+        return self.env.ref('purchase.report_purchase_quotation')
+    def BTW(self):
+        self.write({'state': "approved"})
+        return self.env.ref('purchase.report_purchase_quotation')
 
     @api.onchange('name')
     def _compute_have_write_right(self):
