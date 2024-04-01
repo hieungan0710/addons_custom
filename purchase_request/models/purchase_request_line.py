0ccbaf67cd339b3cdcdabf200ec911a37387ab18
hieungan0710 <160987869+hieungan0710@users.noreply.github.com>
2024-04-01 20:40:37 +0700

    Add files via upload

--- purchase_request/models/purchase_request_line.py
+++ purchase_request/models/purchase_request_line.py
@@ -12,8 +12,16 @@ class PurchaseRequestLine(models.Model):
     qty = fields.Float(string='Số lượng')
     qty_approve = fields.Float(string='Số lượng đã phê duyệt',invisible=True)
     total = fields.Float(string='Tổng', compute='_compute_total', store=True)
-
+    price_unit = fields.Float(string='Đơn Giá')
     @api.depends('qty', 'product_id.list_price')
     def _compute_total(self):
         for line in self:
             line.total = line.qty * line.product_id.list_price
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+       if self.product_id:
+           self.uom_id = self.product_id.uom_id.id #tự dộng lấy đơn vị tính của sản phận
+           # Tự động lấy giá gần nhất từ lịch sử bảng giá mua
+           supplier_info = self.product_id.seller_ids.filtered(lambda s: s.name.id == self.request_id.approver_id.id)
+           if supplier_info:
+               self.list_pice = supplier_info.price_unit
