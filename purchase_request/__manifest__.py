239185eebd323b60b71e5c42a4375b2b029441c2
hieungan0710 <160987869+hieungan0710@users.noreply.github.com>
2024-04-03 17:40:20 +0700

    Add files via upload

--- purchase_request/__manifest__.py
+++ purchase_request/__manifest__.py
@@ -6,12 +6,13 @@
     'website': "https://www.ngannh.com",
     'category': 'Uncategorized',
     'version': '0.1',
-    'depends': ['purchase', 'hr', 'product'],
+    'depends': ['purchase', 'hr', 'product', 'sale'],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
         'data/purchase_sequence.xml',
         'views/purchase_request_views.xml',
         'views/purchase_request_line_views.xml',
+        'data/purchase_request_demo_data.xml',
     ],
 }
