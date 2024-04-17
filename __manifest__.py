30befdb8176eaf8856feceb3ab920c353f9ce6b5
hieungan0710 <160987869+hieungan0710@users.noreply.github.com>
2024-04-17 10:33:51 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': " Ev Event",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base','event'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        'views/ev_event_view.xml',
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
